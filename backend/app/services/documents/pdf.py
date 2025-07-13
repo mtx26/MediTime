122366de8ae7e89f84bc3e9d8f372e87935137a7
mtx26 <134505471+mtx26@users.noreply.github.com>
2025-07-13 22:34:44 +0200

    Clean duplicate imports

--- backend/app/services/documents/pdf.py
+++ backend/app/services/documents/pdf.py
@@ -7,18 +7,7 @@ from app.services.medication import get_medicines_for_calendar
 moment_map = {
     "morning": "matin",
     "noon": "midi",
-    "evening": "soir"
-}
-
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.styles import getSampleStyleSheet
-from io import BytesIO
-
-moment_map = {
-    "morning": "matin",
-    "noon": "midi",
-    "evening": "soir"
+    "evening": "soir",
 }
 
 def generate_medicine_conditions_pdf(calendar_id):
