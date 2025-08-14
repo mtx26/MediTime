a0396791ce636825e5ebfaa93ee81d930cf31977
mtx26 <134505471+mtx26@users.noreply.github.com>
2025-08-14 16:51:18 +0200

    fix: prevent repeated language prefixes

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -1,37 +1,39 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from 'react-router-dom';
 import App from './App';
 import { useTranslation } from 'react-i18next';
 import { enabledLanguageCodes, LANGUAGES } from './config/languages';
 
-function RedirectToDefaultLang() {
+function LanguageRoutes() {
   const location = useLocation();
   const { i18n } = useTranslation();
   const defaultLang = i18n.language || LANGUAGES[0].code;
-  const path = location.pathname + location.search + location.hash;
-  return <Navigate to={`/${defaultLang}${path}`} replace />;
+  const pathParts = location.pathname.split('/');
+  const currentLang = pathParts[1];
+
+  if (!enabledLanguageCodes.includes(currentLang)) {
+    const path = location.pathname + location.search + location.hash;
+    return <Navigate to={`/${defaultLang}${path}`} replace />;
+  }
+
+  return (
+    <Routes>
+      <Route path="/:lng/*" element={<App />} />
+    </Routes>
+  );
 }
 
-function Root() {
-  const pathParts = window.location.pathname.split('/');
-  const currentLang = pathParts[1];
-  if (!enabledLanguageCodes.includes(currentLang)) {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<RedirectToDefaultLang />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+export default function Root() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/:lng/*" element={<App />} />
-        <Route path="*" element={<RedirectToDefaultLang />} />
-      </Routes>
+      <LanguageRoutes />
     </BrowserRouter>
   );
 }
 
-export default Root;
