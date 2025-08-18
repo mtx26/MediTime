78a8275491937cd040d593923fa6b7d813d4d08f
mtx26 <mtx12345626@gmail.com>
2025-08-18 20:44:47 +0200

    Amélioration de la structure et de la lisibilité de la liste des calendriers

--- frontend/src/pages/calendar/CalendarList.jsx
+++ frontend/src/pages/calendar/CalendarList.jsx
@@ -144,181 +144,177 @@ function SelectCalendar({
         {/* Liste des calendriers */}
         <div className="list-group shadow">
           {(Array.isArray(personalCalendars.calendarsData) && personalCalendars.calendarsData.length > 0) && (
-            <>
-              {personalCalendars.calendarsData.map((calendarData, index) => (
-                <div key={index} className="list-group-item">
-                  {selectedAlert === 'calendar' + calendarData.id && (
-                    <AlertSystem
-                      type={alertType}
-                      message={alertMessage}
-                      onClose={() => {
-                        setAlertMessage('');
-                        setOnConfirmAction(null);
-                        setSelectedAlert(null);
-                      }}
-                      onConfirm={() => {
-                        if (onConfirmAction) onConfirmAction();
-                      }}
-                    />
-                  )}
-                  <div className="d-flex flex-wrap justify-content-between align-items-center gap-2 mb-2">
-                    {/* Partie gauche : Nom + nombre */}
-                    <div className="me-auto">
-                      <h5 className="mb-1 fs-semibold">{calendarData.name}</h5>
-                      <div className="text-muted small">
-                        {t('medicines.label')}:
-                        <span className="fw-semibold ms-1">
-                          {calendarData.boxesCount ?? '...'}
+            personalCalendars.calendarsData.map((calendarData, index) => (
+              <div key={index} className="list-group-item">
+                {selectedAlert === 'calendar' + calendarData.id && (
+                  <AlertSystem
+                    type={alertType}
+                    message={alertMessage}
+                    onClose={() => {
+                      setAlertMessage('');
+                      setOnConfirmAction(null);
+                      setSelectedAlert(null);
+                    }}
+                    onConfirm={() => {
+                      if (onConfirmAction) onConfirmAction();
+                    }}
+                  />
+                )}
+                <div className="d-flex flex-wrap justify-content-between align-items-center gap-2 mb-2">
+                  {/* Partie gauche : Nom + nombre */}
+                  <div className="me-auto">
+                    <h5 className="mb-1 fs-semibold">{calendarData.name}</h5>
+                    <div className="text-muted small">
+                      {t('medicines.label')}:
+                      <span className="fw-semibold ms-1">
+                        {calendarData.boxesCount ?? '...'}
+                      </span>
+                    </div>
+                    {calendarData.ifLowStock && (
+                      <button className="btn p-0" onClick={() => navigate(`/${lng}/calendar/${calendarData.id}/stock-alerts`)}>
+                        <span className="badge bg-warning d-flex align-items-center gap-1">
+                          <i className='bi bi-exclamation-triangle-fill'></i>{t('stock_alert')}
                         </span>
-                      </div>
-                      {calendarData.ifLowStock && (
-                        <button className="btn p-0" onClick={() => navigate(`/${lng}/calendar/${calendarData.id}/stock-alerts`)}>
-                          <span className="badge bg-warning d-flex align-items-center gap-1">
-                            <i className='bi bi-exclamation-triangle-fill'></i>{t('stock_alert')}
-                          </span>
-                        </button>
-                      )}
-                    </div>
-
-                    {/* Bouton Ouvrir */}
-                    <button
-                      className="btn btn-outline-success"
-                      title={t('open')}
-                      aria-label={t('open')}
-                      onClick={() => navigate(`/${lng}/calendar/${calendarData.id}`)}
-                    >
-                      {t('open')}
-                    </button>
-
-                    {/* ActionSheet */}
-                    <ActionSheet
-                      actions={[
-                        {
-                          label: (
-                            <>
-                              <i className="bi bi-pencil me-2"></i> {t('rename')}
-                            </>
-                          ),
-                          onClick: () => setRenameMode(calendarData.id),
-                        },
-                        { separator: true },
-                        {
-                          label: (
-                            <>
-                              <i className="bi bi-box-arrow-up me-2"></i> {t('share')}
-                            </>
-                          ),
-                          onClick: () => navigate(`/${lng}/shared-calendars?calendar=${calendarData.id}`),
-                        },
-                        { separator: true },
-                        {
-                          label: (
-                            <>
-                              <i className="bi bi-capsule me-2"></i> {t('medicines.label')}
-                            </>
-                          ),
-                          onClick: () => navigate(`/${lng}/calendar/${calendarData.id}/boxes`),
-                        },
-                        {
-                          label: (
-                            <>
-                              <i className="bi bi-download me-2"></i> {t('boxes.export_pdf')}
-                            </>
-                          ),
-                          onClick: () => personalCalendars.downloadPersonalCalendarPdf(calendarData.id),
-                        },
-                        {
-                          label: (
-                            <>
-                              <i className="bi bi-exclamation-triangle-fill me-2"></i> {t('stock')}
-                            </>
-                          ),
-                          onClick: () =>
-                            navigate(`/${lng}/calendar/${calendarData.id}/stock-alerts`),
-                        },
-                        { separator: true },
-                        {
-                          label: (
-                            <>
-                              <i className="bi bi-gear me-2"></i> {t('settings.label')}
-                            </>
-                          ),
-                          onClick: () =>
-                            navigate(`/${lng}/calendar/${calendarData.id}/settings`),
-                        },
-                        { separator: true },
-                        {
-                          label: (
-                            <>
-                              <i className="bi bi-trash me-2"></i> {t('delete')}
-                            </>
-                          ),
-                          onClick: () => handleDeleteCalendarClick(calendarData.id),
-                          danger: true,
-                        },
-                      ]}
-                    />
-
+                      </button>
+                    )}
                   </div>
-                  {/* afficher la form si on est en mode renommage */}
-                  {renameMode === calendarData.id && (
-                    <div className="d-flex justify-content-center">
-                      {/* Partie pour renommer un calendrier */}
-                      <form
-                        className="input-group input-group w-100 w-md-auto"
-                        onSubmit={(e) => {
-                          e.preventDefault();
-                          handleRenameClick(calendarData.id);
-                          setRenameMode(null);
-                        }}
-                      >
-                        <input
-                          id={'renameCalendarName' + calendarData.id}
-                          aria-label={t('calendar.new_name')}
-                          type="text"
-                          className="form-control form-control"
-                          placeholder={t('calendar.new_name')}
-                          required
-                          value={renameValues[calendarData.id] || ''} // Valeur du champ de renommage
-                          onChange={(e) =>
-                            setRenameValues({
-                              ...renameValues,
-                              [calendarData.id]: e.target.value,
-                            })
-                          } // Mise à jour de l'état
-                        />
-                        <button
-                          className="btn btn-warning"
-                          title={t('rename')}
-                          type="submit"
-                          aria-label={t('rename')}
-                        >
-                          <i className="bi bi-pencil"></i>
-                        </button>
-                        <button
-                          className="btn btn-outline-danger"
-                          title={t('cancel')}
-                          type="button"
-                          aria-label={t('cancel')}
-                          onClick={() => setRenameMode(null)}
-                        >
-                          <i className="bi bi-x-lg"></i>
-                        </button>
-                      </form>
-                    </div>
-                  )}
-                </div>
-              ))}
 
-              <button
-                className="text-center btn btn-outline-primary rounded-0 rounded-bottom"
-                onClick={() => navigate(`/${lng}/add-calendar`)}
-              >
-                <i className="bi bi-calendar-plus me-2"></i> {t('calendar.add_calendar')}
-              </button>
-            
-            </>
+                  {/* Bouton Ouvrir */}
+                  <button
+                    className="btn btn-outline-success"
+                    title={t('open')}
+                    aria-label={t('open')}
+                    onClick={() => navigate(`/${lng}/calendar/${calendarData.id}`)}
+                  >
+                    {t('open')}
+                  </button>
+
+                  {/* ActionSheet */}
+                  <ActionSheet
+                    actions={[
+                      {
+                        label: (
+                          <>
+                            <i className="bi bi-pencil me-2"></i> {t('rename')}
+                          </>
+                        ),
+                        onClick: () => setRenameMode(calendarData.id),
+                      },
+                      { separator: true },
+                      {
+                        label: (
+                          <>
+                            <i className="bi bi-box-arrow-up me-2"></i> {t('share')}
+                          </>
+                        ),
+                        onClick: () => navigate(`/${lng}/shared-calendars?calendar=${calendarData.id}`),
+                      },
+                      { separator: true },
+                      {
+                        label: (
+                          <>
+                            <i className="bi bi-capsule me-2"></i> {t('medicines.label')}
+                          </>
+                        ),
+                        onClick: () => navigate(`/${lng}/calendar/${calendarData.id}/boxes`),
+                      },
+                      {
+                        label: (
+                          <>
+                            <i className="bi bi-download me-2"></i> {t('boxes.export_pdf')}
+                          </>
+                        ),
+                        onClick: () => personalCalendars.downloadPersonalCalendarPdf(calendarData.id),
+                      },
+                      {
+                        label: (
+                          <>
+                            <i className="bi bi-exclamation-triangle-fill me-2"></i> {t('stock')}
+                          </>
+                        ),
+                        onClick: () =>
+                          navigate(`/${lng}/calendar/${calendarData.id}/stock-alerts`),
+                      },
+                      { separator: true },
+                      {
+                        label: (
+                          <>
+                            <i className="bi bi-gear me-2"></i> {t('settings.label')}
+                          </>
+                        ),
+                        onClick: () =>
+                          navigate(`/${lng}/calendar/${calendarData.id}/settings`),
+                      },
+                      { separator: true },
+                      {
+                        label: (
+                          <>
+                            <i className="bi bi-trash me-2"></i> {t('delete')}
+                          </>
+                        ),
+                        onClick: () => handleDeleteCalendarClick(calendarData.id),
+                        danger: true,
+                      },
+                    ]}
+                  />
+
+                </div>
+                {/* afficher la form si on est en mode renommage */}
+                {renameMode === calendarData.id && (
+                  <div className="d-flex justify-content-center">
+                    {/* Partie pour renommer un calendrier */}
+                    <form
+                      className="input-group input-group w-100 w-md-auto"
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        handleRenameClick(calendarData.id);
+                        setRenameMode(null);
+                      }}
+                    >
+                      <input
+                        id={'renameCalendarName' + calendarData.id}
+                        aria-label={t('calendar.new_name')}
+                        type="text"
+                        className="form-control form-control"
+                        placeholder={t('calendar.new_name')}
+                        required
+                        value={renameValues[calendarData.id] || ''} // Valeur du champ de renommage
+                        onChange={(e) =>
+                          setRenameValues({
+                            ...renameValues,
+                            [calendarData.id]: e.target.value,
+                          })
+                        } // Mise à jour de l'état
+                      />
+                      <button
+                        className="btn btn-warning"
+                        title={t('rename')}
+                        type="submit"
+                        aria-label={t('rename')}
+                      >
+                        <i className="bi bi-pencil"></i>
+                      </button>
+                      <button
+                        className="btn btn-outline-danger"
+                        title={t('cancel')}
+                        type="button"
+                        aria-label={t('cancel')}
+                        onClick={() => setRenameMode(null)}
+                      >
+                        <i className="bi bi-x-lg"></i>
+                      </button>
+                    </form>
+                  </div>
+                )}
+              </div>
+            ))
           )}
+          <button
+            className={`text-center btn btn-outline-primary ${personalCalendars.calendarsData.length > 0 ? 'rounded-0 rounded-bottom' : ''}`}
+            onClick={() => navigate(`/${lng}/add-calendar`)}
+          >
+            <i className="bi bi-calendar-plus me-2"></i> {t('calendar.add_calendar')}
+          </button>
         </div>
       </div>
 
