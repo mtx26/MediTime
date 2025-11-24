32ce03dedd347a6ec6a20ebef27b4fed4d6aa559
mtx26 <mtx12345626@gmail.com>
2025-11-24 19:38:27 +0100

    fix: corriger l'extracteur par défaut pour le plugin purgecss

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -4,7 +4,7 @@ export default {
   plugins: [
     purgecss.default({
       content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+      defaultExtractor: (content) => content.match(/[\w-/:]+/g) || [],
       safelist: {
         standard: [/^btn-/, /^text-/, /^bg-/, /^modal-/, /^fade/, /^show/], // Common Bootstrap patterns to be safe
         deep: [],
