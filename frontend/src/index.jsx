a2609563fba6789f5336f5eed48fa3692d47d9a5
mtx26 <mtx12345626@gmail.com>
2025-11-24 17:26:18 +0100

    feat: Ajouter le package @vercel/speed-insights et l'intégrer dans l'application

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,10 +4,12 @@ import './styles/index.css';
 import Root from './Root';
 import { UserProvider } from './contexts/UserContext';
 import './i18n';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <UserProvider>
     <Root />
+    <SpeedInsights />
   </UserProvider>,
 );
