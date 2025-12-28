a16bd0c095fb11cc10f70b3e05a9ee5e41ae8b7f
mtx26 <mtx12345626@gmail.com>
2025-12-28 20:26:11 +0100

    fix: ajouter ScrollArea pour améliorer la mise en page de l'application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1069,6 +1069,7 @@ function App() {
     <div className="flex flex-col min-h-screen">
       <OnboardingTour isAppLoading={isInitialLoading} />
       <Navbar sharedProps={sharedProps} />
+      <ScrollArea className="h-full w-full grow flex flex-col mb-24 lg:mb-0">
         <main className="flex-grow flex flex-col">
           {userInfo && (
             <RealtimeManager
@@ -1087,6 +1088,7 @@ function App() {
           </div>
         </main>
         <Footer />
+      </ScrollArea>
       <MobileNavBar/>
     </div>
   );
