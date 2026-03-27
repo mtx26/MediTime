4fb16270ceda8b3517518145fb88390e2d8d021b
mtx26 <mtx12345626@gmail.com>
2026-03-27 10:04:02 +0100

    feat(i18n): add translation resources and update package dependencies

---
+++ packages/i18n/src/resources.js
@@ -0,0 +1,21 @@
+import de from './locales/de/translation.json';
+import en from './locales/en/translation.json';
+import es from './locales/es/translation.json';
+import fr from './locales/fr/translation.json';
+import it from './locales/it/translation.json';
+import ja from './locales/ja/translation.json';
+import pt from './locales/pt/translation.json';
+import ru from './locales/ru/translation.json';
+import zh from './locales/zh/translation.json';
+
+export const translationResources = {
+  de: { translation: de },
+  en: { translation: en },
+  es: { translation: es },
+  fr: { translation: fr },
+  it: { translation: it },
+  ja: { translation: ja },
+  pt: { translation: pt },
+  ru: { translation: ru },
+  zh: { translation: zh },
+};
